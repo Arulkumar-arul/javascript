a34893347fc74b2d58e4e5ba23e68c5c88fd6581
Arulkumar-arul <163402868+Arulkumar-arul@users.noreply.github.com>
2026-08-30 15:41:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var name =prompt("what is your name")
+
+
+var age = prompt("what is your age")
+
+
+var mark = prompt("what is your mark")
+
+console.log(name,age,mark)
+
+alert(name)
+const qoes= "did you coding?"
+confirm(qoes)
+document.write(name,age,mark)
